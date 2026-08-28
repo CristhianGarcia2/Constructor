686d8fd9f572c831bc93d858bcda81a88d823a7f
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-08-28 18:58:32 -0500

    definimos los atributos principales de la clase y del metodo constructor y  de los difrentes getters

--- src/Animales.java
+++ src/Animales.java
@@ -3,4 +3,27 @@ public class Animales {
     private int edad;
     private String peso;
     private String raza;
+
+    public Animales(String nombre, int edad, String peso, String raza) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
 }
