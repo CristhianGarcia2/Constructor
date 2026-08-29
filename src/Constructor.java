adebac6c34d5bea0cef04fd5645bab03b1215cd0
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-08-28 19:11:43 -0500

    Se agregan la  impresion formateada de los datos de los diferentes animales

--- src/Constructor.java
+++ src/Constructor.java
@@ -4,5 +4,16 @@ public class Constructor {
         Leon leon = new Leon("Simba", 5, "190kg", "Leon del Congo");
         Lobo lobo = new Lobo("Ghost", 4, "40kg", "Lobo Gris");
         Buho buho = new Buho("Archimedes", 2, "2kg", "Buho Real");
+
+
+        System.out.println("Nombre del leon: " + leon.getNombre());
+        System.out.println("Edad del leon: " + leon.getEdad() + " anos");
+
+        System.out.println("Nombre del lobo: " + lobo.getNombre());
+        System.out.println("Edad del lobo: " + lobo.getEdad() + " anos");
+
+        System.out.println("Nombre del buho: " + buho.getNombre());
+        System.out.println("Edad del buho: " + buho.getEdad() + " anos");
     }
 }
+
