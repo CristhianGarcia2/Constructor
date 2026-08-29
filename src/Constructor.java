d810f7754a59933faab4005c99fe5d39ebcaa3d0
CristhianGarcia2 <cgarcia@uniempresarial.edu.co>
2026-08-28 19:10:10 -0500

    Se instancian los animales Leon, Lobo y Buho en la clase main

---
+++ src/Constructor.java
@@ -0,0 +1,8 @@
+public class Constructor {
+
+    public static void main(String[] args) {
+        Leon leon = new Leon("Simba", 5, "190kg", "Leon del Congo");
+        Lobo lobo = new Lobo("Ghost", 4, "40kg", "Lobo Gris");
+        Buho buho = new Buho("Archimedes", 2, "2kg", "Buho Real");
+    }
+}
